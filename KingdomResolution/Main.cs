5478ea4ff32a093a2219dd06f1c20d3fca4e2c29
spacehamster <mail.spacehamster@gmail.com>
2018-10-21 13:17:40 +1100

    First

---
+++ KingdomResolution/Main.cs
@@ -0,0 +1,61 @@
+﻿using Harmony12;
+using UnityModManagerNet;
+using System.Reflection;
+using System;
+using Debug = System.Diagnostics.Debug;
+using System.Diagnostics;
+using Kingmaker.Kingdom.Tasks;
+
+namespace KingdomResolution
+{
+
+    public class Main
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        private static void DebugLog(string msg)
+    => Debug.WriteLine(nameof(KingdomResolution) + ": " + msg);
+
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Debug.Listeners.Add(new TextWriterTraceListener("Mods/KingdomResolution/KingdomResolution.log"));
+            Debug.AutoFlush = true;
+
+            var harmony = HarmonyInstance.Create(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+        // Called when the mod is turned to on/off.
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value /* active or inactive */)
+        {
+            enabled = value;
+            return true; // Permit or not.
+        }
+        private static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+        }
+        [HarmonyPatch(typeof(KingdomEvent), "CalculateRulerTime")]
+        static class KingdomEvent_CalculateRulerTime_Patch
+        {
+            static bool Prefix(ref int __result)
+            {
+                if (!enabled) return true;
+                __result = 0;
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(KingdomEvent), "CalculateResolutionTime")]
+        static class KingdomEvent_CalculateResolutionTime_Patch
+        {
+            static bool Prefix(KingdomEvent __instance, ref int __result)
+            {
+                if (!enabled) return true;
+                if (__instance.EventBlueprint.NeedToVisitTheThroneRoom) return true;
+                __result = 1;
+                return false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# KingmakerKingdomResolutionMod
+Mod to remove backpacks, helmets and change class outfits for the Game Pathfinder Kingmaker.
+## Install
+1. Download and install Unity Mod Manager﻿
+2. Download the [mod](https://github.com/spacehamster/KingmakerVisualAdjustmentsMod/releases)
+3. Extract the archive and put the mod folder into 'Mods' folder of the Game (\Steam\steamapps\common\Pathfinder Kingmaker\Mods).
