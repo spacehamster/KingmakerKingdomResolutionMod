cd9549f362d8fba7cd4f590ad715db5f6709070a
spacehamster <mail.spacehamster@gmail.com>
2018-10-21 14:28:52 +1100

    Added Event DC Modifier, show Baron portrait on Baron Events

--- KingdomResolution/Main.cs
+++ KingdomResolution/Main.cs
@@ -5,6 +5,7 @@ using System;
 using Debug = System.Diagnostics.Debug;
 using System.Diagnostics;
 using Kingmaker.Kingdom.Tasks;
+using UnityEngine;
 
 namespace KingdomResolution
 {
@@ -16,15 +17,18 @@ namespace KingdomResolution
     => Debug.WriteLine(nameof(KingdomResolution) + ": " + msg);
 
         public static bool enabled;
+        static Settings settings;
 
         static bool Load(UnityModManager.ModEntry modEntry)
         {
             Debug.Listeners.Add(new TextWriterTraceListener("Mods/KingdomResolution/KingdomResolution.log"));
             Debug.AutoFlush = true;
-
+            settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
             var harmony = HarmonyInstance.Create(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
             return true;
         }
         // Called when the mod is turned to on/off.
@@ -33,16 +37,38 @@ namespace KingdomResolution
             enabled = value;
             return true; // Permit or not.
         }
-        private static void OnGUI(UnityModManager.ModEntry modEntry)
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
+            settings.Save(modEntry);
         }
-        [HarmonyPatch(typeof(KingdomEvent), "CalculateRulerTime")]
-        static class KingdomEvent_CalculateRulerTime_Patch
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            if (!enabled) return;
+            settings.skipEvents = GUILayout.Toggle(settings.skipEvents, "Enable 1 Day Events ", GUILayout.ExpandWidth(false));
+            GUILayout.BeginHorizontal(Array.Empty<GUILayoutOption>());
+            GUILayout.Label("Event DC Modifier ", GUILayout.ExpandWidth(false));
+            settings.DCModifier = (int)GUILayout.HorizontalSlider(settings.DCModifier, -100, 100, GUILayout.Width(300f));
+            GUILayout.Label(" " + settings.DCModifier, GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+        }
+        [HarmonyPatch(typeof(KingdomTaskEvent), "SkipPlayerTime", MethodType.Getter)]
+        static class KingdomTaskEvent_SkipPlayerTime_Patch
         {
             static bool Prefix(ref int __result)
             {
                 if (!enabled) return true;
+                if (!settings.skipEvents) return true;
                 __result = 0;
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(KingdomEvent), "DCModifier", MethodType.Getter)]
+        static class KingdomEvent_DCModifier_Patch
+        {
+            static bool Prefix(ref int __result)
+            {
+                if (!enabled) return true;
+                __result = settings.DCModifier;
                 return false;
             }
         }
@@ -52,6 +78,7 @@ namespace KingdomResolution
             static bool Prefix(KingdomEvent __instance, ref int __result)
             {
                 if (!enabled) return true;
+                if (!settings.skipEvents) return true;
                 if (__instance.EventBlueprint.NeedToVisitTheThroneRoom) return true;
                 __result = 1;
                 return false;
---
+++ KingdomResolution/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace KingdomResolution
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool skipEvents = true;
+        public int DCModifier = 0;
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
